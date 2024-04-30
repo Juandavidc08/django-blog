08433858c2743b03951cac705bb0369dbbbb91f2
JD Camelo <jdavidcamelo94@gmail.com>
2024-04-30 17:46:39 +0000

    Add envirnment for security

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,6 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-73m+u=9__^!4#v1owkhb5#3%ii!30k6g)$)zc2@xei%9swka)3'
 
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
